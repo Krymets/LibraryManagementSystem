37365f3bc897eda2541d1ebf5a272454249e6b67
Alexander <akrimec@gmail.com>
2025-05-27 12:36:04 +0300

    Set default SECRET_KEY and add .simple_env file for configs
    Added a temporary hardcoded SECRET_KEY for local use and introduced a .simple_env file to manage environment variables. This ensures a more modular setup, though hardcoded keys should be replaced before production deployment.

--- library_system/settings.py
+++ library_system/settings.py
@@ -11,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-7y96ct7vfqbjqy945ask54rz4i5u65q1yynebpuo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
